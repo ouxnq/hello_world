9f542409314b07da6446fe2fdbbcbfb2d2292659
ouxnq <buenop.vinicius@gmail.com>
2024-07-01 15:32:28 -0300

    todos os códigos em uma unica class

--- src/main/java/com/generation/helloworld/controller/HelloWorldController.java
+++ src/main/java/com/generation/helloworld/controller/HelloWorldController.java
@@ -5,11 +5,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/hello-world")
+@RequestMapping("/API")
 public class HelloWorldController {
 	
-	@GetMapping
+	@GetMapping("/hello-world")
 	public String helloWorld() {
 		return "Hello World!!!";
+	}
+	@GetMapping("/bsm")
+	public String bsm() {
+		return "MENTALIDADES:\n1-Orientação ao futuro\n2-Responsabilidade Pessoal\n3-Mentalidade de Crescimento\n4-Persistência\n\nHABILIDADES:\n1-Trabalho em Equipe\n2-Atenção aos Detalhes\n3-Proatividade\n4-Comunicação";
+	
+	}
+	@GetMapping("obj")
+	public String objetivo() {
+		return "Aprender com maestria tudo sobre SpringBoot!!";
 	}
 }
